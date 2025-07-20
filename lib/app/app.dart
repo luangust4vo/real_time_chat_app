d0c5ff0d506b3369342ff3224f8cb8539c5dac7b
Luan Gustavo <gluan1213@gmail.com>
2025-07-19 23:16:32 -0300

    feat: implement auth state redirection

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -8,6 +8,7 @@ import 'package:real_time_chat_app/features/auth/screens/register.dart';
 import 'package:real_time_chat_app/features/chat/screens/chat_list.dart';
 import 'package:real_time_chat_app/features/settings/screens/settings.dart';
 import 'package:real_time_chat_app/providers/theme_provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -16,24 +17,39 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<ThemeProvider>(
       builder: (context, themeProvider, child) {
-        return MaterialApp(
-          title: 'Real Time Chat App',
-          theme: ThemeData(
-            primaryColor: Colors.blueGrey,
-            brightness:
-                themeProvider.isDarkMode ? Brightness.dark : Brightness.light,
-          ),
-          debugShowCheckedModeBanner: false,
-          initialRoute: Routes.home,
-          routes: {
-            Routes.home: (context) => const Welcome(),
-            Routes.chat: (context) => const Chat(),
-            Routes.chat_list: (context) => ChatList(),
-            Routes.login: (context) => Login(),
-            Routes.register: (context) => Register(),
-            Routes.settings: (context) => const Settings(),
-          },
-        );
+        return StreamBuilder<AuthState>(
+            stream: Supabase.instance.client.auth.onAuthStateChange,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const MaterialApp(
+                  home: Scaffold(
+                    body: Center(child: CircularProgressIndicator()),
+                  ),
+                );
+              }
+
+              final hasUser =
+                  snapshot.hasData && snapshot.data?.session != null;
+
+              return MaterialApp(
+                title: 'Real Time Chat App',
+                theme: ThemeData(
+                  primaryColor: Colors.blueGrey,
+                  brightness: themeProvider.isDarkMode
+                      ? Brightness.dark
+                      : Brightness.light,
+                ),
+                debugShowCheckedModeBanner: false,
+                home: hasUser ? ChatList() : const Welcome(),
+                routes: {
+                  Routes.chat: (context) => const Chat(),
+                  Routes.chat_list: (context) => ChatList(),
+                  Routes.login: (context) => Login(),
+                  Routes.register: (context) => Register(),
+                  Routes.settings: (context) => const Settings(),
+                },
+              );
+            });
       },
     );
   }
